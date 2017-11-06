3b4663e8c39b7f9804ec8b305cdbc56749d2be41
vagrant <vagrant@homestead>
2017-11-06 12:58:49 +0000

    User profile page

---
+++ app/Http/Controllers/Everan/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Everan;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 Auth::routes();
 
 Route::group(['namespace' => 'Everan'], function () {
+
     Route::get('/', 'PagesController@root')->name('root');
+
+    Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
 });
