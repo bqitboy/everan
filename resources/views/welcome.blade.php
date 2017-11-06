9585f6f04cbbaebc134c1622a131923ac95db305
dangran <bqitboy.qq.com>
2017-11-06 09:07:47 +0800

    layuots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,6 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         <!-- Styles -->
         <style>
             html, body {
@@ -79,7 +76,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    I am Everan ！(*^__^*)
                 </div>
 
                 <div class="links">
