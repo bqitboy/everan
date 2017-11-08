41119c77fbd1090b31898932d92bafef3f4321c4
vagrant <vagrant@homestead>
2017-11-08 13:24:34 +0000

    Seeding topics and users data

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::group(['namespace' => 'Everan'], function () {
 
     Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-});
+    Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+});
