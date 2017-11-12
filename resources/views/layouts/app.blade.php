a649eca85ac8576e52e6d0961ae199f28a18a031
vagrant <vagrant@homestead>
2017-11-12 02:58:09 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Everan') - Laravel 进阶教程</title>
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description', 'Everan 爱好者社区')" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}?v=1.0.2" rel="stylesheet">
